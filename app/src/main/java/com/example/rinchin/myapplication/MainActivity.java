288a4a7fdda4a19449e9d91110a7cdb9cae60e5d
Rinchin <gomboev.rinchin@gmail.com>
2016-06-26 10:22:19 +0300

    Last Working Commit

--- app/src/main/java/com/example/rinchin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rinchin/myapplication/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.rinchin.myapplication;
 
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -20,10 +28,17 @@ import android.widget.TextView;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.gms.appdatasearch.GetRecentContextCall;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -36,6 +51,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -58,11 +74,25 @@ import com.android.volley.*;
 
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback,
+        GoogleMap.OnMapClickListener,GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
     ArrayList<myPoint> listLatLng = new ArrayList<>();
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
+    public MainActivity() {
+        // Add a marker in Array and
+        listLatLng.add(new myPoint("Москва Билайн", new LatLng(55.773596, 37.609142), "Билайн"));
+        listLatLng.add(new myPoint("Test", new LatLng(55.773716, 37.606313), "Test"));
+        listLatLng.add(new myPoint("Test2", new LatLng(55.774452, 37.606320), "Test2"));
+
+
+    }
 
     String result = "";
     static final String url = "http://sasha.starkov-it.ru/vint/serv.php";
@@ -86,7 +116,7 @@ public class MainActivity extends AppCompatActivity
                 inputStream = httpResponse.getEntity().getContent();
 
                 // convert inputstream to string
-                if(inputStream != null)
+                if (inputStream != null)
                     result = convertInputStreamToString(inputStream);
                 else
                     result = "Did not work!";
@@ -100,11 +130,11 @@ public class MainActivity extends AppCompatActivity
     };
 
     public View view = null;
-    Handler handler = new Handler(){
+    Handler handler = new Handler() {
 
 
         @Override
-        public void handleMessage(Message msg){
+        public void handleMessage(Message msg) {
             Bundle bundle = msg.getData();
             String result = bundle.getString("Key");
             Snackbar.make(view, result, Snackbar.LENGTH_LONG)
@@ -124,14 +154,61 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-               //         .setAction("Action", null).show();
+                // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //         .setAction("Action", null).show();
+
+                //clear GMap
+                mMap.clear();
+                //listLatLng.clear();
+
                 view = v;
                 new Thread(runnable).start();
 
+                try {
+
+                    String newString = "\"Hello";
+                    String newString1 = newString.replaceAll("\"", "\\\\");
+
+
+                    result = result.replaceAll("\"", "\\\"");
+                    // JSONObject jsonRootObject = new JSONObject(result);
+
+
+                    //Get the instance of JSONArray that contains JSONObjects
+                    // JSONArray jsonArray = jsonRootObject.optJSONArray("Employee");
+                    JSONArray jsonArray = new JSONArray(result);
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        String name = jsonObject.optString("maker_name");
+                        double latitude = Double.parseDouble(jsonObject.optString("maker_lat"));
+                        double lagutide = Double.parseDouble(jsonObject.optString("maker_lag"));
+                        String comments = jsonObject.optString("maker_text");
+                        listLatLng.add(new myPoint(name, latitude, lagutide, comments));
+                        //mMap.addMarker(new MarkerOptions().position(new LatLng(latitude,lagutide))
+                        //      .title(name));
+
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
 
                 Snackbar.make(view, result, Snackbar.LENGTH_LONG)
-                         .setAction("Action", null).show();
+                        .setAction("Action", null).show();
+
+                // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+                for (myPoint point : listLatLng) {
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(point.latLng.latitude, point.latLng.longitude))
+                            .title(point.Name));
+                }
+
+
+                mMap.addMarker(new MarkerOptions().position(new LatLng(1, 1)).title("Test"));
+                SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+                mapFragment.getMapAsync(MainActivity.this);
             }
 
 
@@ -153,14 +230,17 @@ public class MainActivity extends AppCompatActivity
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
         String line = "";
         String result = "";
-        while((line = bufferedReader.readLine()) != null)
+        while ((line = bufferedReader.readLine()) != null)
             result += line;
 
         inputStream.close();
@@ -177,25 +257,66 @@ public class MainActivity extends AppCompatActivity
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Array and
-        listLatLng.add(new myPoint("Москва Билайн",new LatLng(55.773596, 37.609142),"Билайн"));
-        listLatLng.add(new myPoint("Test",new LatLng(55.773716, 37.606313),"Test"));
-        listLatLng.add(new myPoint("Test2",new LatLng(55.774452, 37.606320),"Test2"));
 
-        for (myPoint point :listLatLng) {
+        //Initialize Google Play Services
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                buildGoogleApiClient();
+                mMap.setMyLocationEnabled(true);
+            }
+        }
+        else {
+            buildGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+        }
+        for (myPoint point : listLatLng) {
             mMap.addMarker(new MarkerOptions().position(point.latLng).title(point.Name));
         }
 
+
+
+        //Set my location
+        /*if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);*/
+
         //move the camera
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(listLatLng.get(0).latLng, 17));
+        if (listLatLng.isEmpty())
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(listLatLng.get(0).latLng, 17));
+        else
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(55.773596, 37.609142), 17));
+
+
+    }
+
+    GoogleApiClient mGoogleApiClient;
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks((GoogleApiClient.ConnectionCallbacks) this)
+                .addOnConnectionFailedListener((GoogleApiClient.OnConnectionFailedListener) this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
     }
 
     //My own method for adding in Array new Points
-    void addPoints(myPoint point){
+    void addPoints(myPoint point) {
         listLatLng.add(point);
     }
 
@@ -256,19 +377,80 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    @Override
+    public void onMapClick(LatLng point) {
+        listLatLng.add(new myPoint("Test", point, "Test"));
+        mMap.addMarker(new MarkerOptions().position(point));
+    }
 
-    class myPoint{
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.rinchin.myapplication/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.rinchin.myapplication/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+    class myPoint {
         String Name;
         String comments;
         LatLng latLng;
 
-        public myPoint(String Name, double latitude, double longitude, String comments){
+        public myPoint(String Name, double latitude, double longitude, String comments) {
             this.Name = Name;
-            this.latLng = new LatLng(latitude,longitude);
+            this.latLng = new LatLng(latitude, longitude);
             this.comments = comments;
         }
 
-        public myPoint(String Name, LatLng latLng, String comments){
+        public myPoint(String Name, LatLng latLng, String comments) {
             this.Name = Name;
             this.latLng = latLng;
             this.comments = comments;
